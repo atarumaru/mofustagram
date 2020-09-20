1bd45335671325c871ecfb72ee554dc746e2218d
wataru-a-72 <now.or.never34@icloud.com>
2020-09-20 20:04:02 +0900

    非同期化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -9,7 +9,6 @@ class LikesController < ApplicationController
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
-    binding.pry
     @like.destroy
     @likes = Like.where(post_id: params[:post_id])
     get_post
