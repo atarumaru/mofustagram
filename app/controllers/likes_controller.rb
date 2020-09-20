f335dd92c33efac7b5c9786957639726d032691c
wataru-a-72 <now.or.never34@icloud.com>
2020-09-20 19:19:46 +0900

    post_likes_pathの引数変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,13 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(user_id: current_user.id, post_id: params[:post_id])
     @like.save
-    binding.pry
     @likes = Like.where(post_id: params[:post_id])
     get_post
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    binding.pry
     @like.destroy
     @likes = Like.where(post_id: params[:post_id])
     get_post
